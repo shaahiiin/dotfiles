7f01c1927e27d33c22a6871f5405c18d7f3d6ba9
Shahin Mohammad Sali <shaahiinms@gmail.com>
2019-09-10 16:03:51 +0530

    Update zsh-plugins-install.sh

--- scripts/zsh-plugins-install.sh
+++ scripts/zsh-plugins-install.sh
@@ -1,4 +1,7 @@
-# Install zsh plugins
+##### Install zsh plugins
+
+# zsh syntax highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# zsh autosugesstions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
